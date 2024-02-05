ddf6da219cc56ec9f6669a2d2724344b56227369
MarcelobigBird <marcelomuckeiro2022@gmail.com>
2024-02-05 14:54:15 -0300

    keyof e typeof

---
+++ src/A0038-keyof-typeof/A0038-keyof-typeof.ts
@@ -0,0 +1,18 @@
+type ColorsObj = typeof colorsObj;
+type ColorsKeys = keyof ColorsObj;
+
+const colorsObj = {
+  red: 'vermelho',
+  green: 'verde',
+  blue: 'azul',
+  purple: 'roxo',
+};
+
+function translateColor(color: ColorsKeys, colors: ColorsObj) {
+  return colors[color];
+}
+
+console.log(translateColor('red', colorsObj));
+console.log(translateColor('green', colorsObj));
+console.log(translateColor('blue', colorsObj));
+console.log(translateColor('purple', colorsObj));
