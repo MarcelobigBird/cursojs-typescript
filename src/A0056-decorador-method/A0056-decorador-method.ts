5d45a391e013f29d3c309294683fa80bb3fd4ba6
MarcelobigBird <marcelomuckeiro2022@gmail.com>
2024-02-18 01:18:37 -0300

    Decoradores de metódo

---
+++ src/A0056-decorador-method/A0056-decorador-method.ts
@@ -0,0 +1,44 @@
+function decorador(classPrototype: any, nameMethod: string, descriptor: PropertyDescriptor): PropertyDescriptor | void {
+  console.log(classPrototype);
+  console.log(nameMethod);
+  console.log(descriptor);
+  return {
+    value: function (...args: string[]) {
+      return args[0].toUpperCase();
+    },
+  };
+}
+
+export class OnePerson {
+  public firstname: string;
+  public lastname: string;
+  public age: number;
+
+  constructor(firstname: string, lastname: string, age: number) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+  }
+
+  @decorador
+  speek(msg: string): string {
+    return `${this.firstname} ${this.lastname}: ${msg}`;
+  }
+
+  public get firstnameCompleted(): string {
+    return this.firstname + ' ' + this.lastname;
+  }
+
+  public set firstnameCompleted(value: string) {
+    const words = value.split(/\s+/g);
+    const firstname = words.shift();
+    if (!firstname) return;
+    this.firstname = firstname;
+    this.lastname = words.join('');
+  }
+}
+
+console.log();
+const person = new OnePerson('Marcelo', 'Pereira dos Santos', 40);
+
+console.log(person.speek('Hello world!!!'));
